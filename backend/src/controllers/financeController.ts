61ea24885087233ede839e590a20b854d0bb70a6
BrunoLopes <Bruno.Lopes0724@jala.university>
2026-02-03 21:32:11 -0300

    fix(finance): only remove XP when deleting investment income

--- backend/src/controllers/financeController.ts
+++ backend/src/controllers/financeController.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+﻿import { Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { AuthRequest } from '../middlewares/auth';
 import fs from 'fs';
